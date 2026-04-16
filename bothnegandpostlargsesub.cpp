06d6d769947924aad25279383d382d1bc7e5d833
Sammy-0722 <samrana2204@gmail.com>
2026-04-16 22:47:25 +0530

    we done largstsubarry for both ngative and positve

--- bothnegandpostlargsesub.cpp
+++ bothnegandpostlargsesub.cpp
@@ -1,11 +1,11 @@
- #include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int find(vector<int>arr,int n,int k){
-     map<int,int>m;
+int findt (vector<int>sr,int n,int k){
+      map<int,int>m;
       int sum=0;
       int maxlen=0;
       for(int i=0;i<n;i++){
-        sum+=arr[i];
+        sum+=sr[i];
         if(sum==k){
             maxlen =i+1;
         }
@@ -19,11 +19,10 @@ int find(vector<int>arr,int n,int k){
       return maxlen;
     }
     int main(){
-         vector<int>v={3, 2, 1,3,2,4,5};
-    int  n= v.size();
-    int k=6;
-    int result = find(v,n,k);
+        vector<int>v={3,4,5,1,6,7,8};
+        int n= v.size();
+        int k=14;
+        int result = findt(v,n,k);
     cout<<result<<endl;
     return 0;
-     
     }
