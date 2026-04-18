4162dc377208c9c2e7e5904d0dee5132a982f386
Sammy-0722 <samrana2204@gmail.com>
2026-04-18 20:16:25 +0530

    today we do better appraoch

--- majoritylelement.cpp
+++ majoritylelement.cpp
@@ -1,29 +1,41 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int majorityElement(vector<int>& nums) {
+int majorityElement(vector<int> &nums)
+{
+
+    // for(int i = 0; i < n; i++) {
+    //     int count = 0;
+
+    //     for(int j = 0; j < n; j++) {
+    //         if(nums[i] == nums[j]) {
+    //             count++;
+    //         }
+    //     }
+
+    //     if(count > n/7) {
+    //         return nums[i];
+    //     }
+    // }
+
+    // return -1;  // Not needed since majority always exists
+
+    // better appraoch
+
+    unordered_map<int, int> mp;
     int n = nums.size();
-
-    for(int i = 0; i < n; i++) {
-        int count = 0;
-
-        for(int j = 0; j < n; j++) {
-            if(nums[i] == nums[j]) {
-                count++;
-            }
-        }
-
-        if(count > n/2) {
-            return nums[i];
+    for(int num:nums){
+        mp[num]++;
+        if(mp[num]>n/2){
+            return num;
         }
     }
-
-    return -1;  // Not needed since majority always exists
+    return -1;
 }
 
-int main() {
-    vector<int> nums = {2,2,1,1,1,2,2};
+int main()
+{
+    vector<int> nums = {7, 7, 1, 7, 1, 7, 7, 8, 8, 2, 7, 4, 7};
 
     cout << "Majority Element: " << majorityElement(nums);
 
