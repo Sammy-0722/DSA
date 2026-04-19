d93810e2949a2a247cae6c887584e586bea7fd30
Sammy-0722 <samrana2204@gmail.com>
2026-04-19 22:33:46 +0530

    moore voting algo

--- majoritylelement.cpp
+++ majoritylelement.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int majorityElement(vector<int> &nums)
-{
+// int majorityElement(vector<int> &nums)
+// {
 
     // for(int i = 0; i < n; i++) {
     //     int count = 0;
@@ -22,17 +22,34 @@ int majorityElement(vector<int> &nums)
 
     // better appraoch
 
-    unordered_map<int, int> mp;
-    int n = nums.size();
-    for(int num:nums){
-        mp[num]++;
-        if(mp[num]>n/2){
-            return num;
-        }
-    }
-    return -1;
-}
+    // unordered_map<int, int> mp;
+    // int n = nums.size();
+    // for(int num:nums){
+    //     mp[num]++;
+    //     if(mp[num]>n/2){
+    //         return num;
+    //     }
+    // }
+    // return -1;
+// }
 
+int majorityElement(vector<int>& nums) {
+    int candidate = 0;
+    int count = 0;
+
+    for(int num : nums) {
+        if(count == 0) {
+            candidate = num;
+        }
+
+        if(num == candidate)
+            count++;
+        else
+            count--;
+    }
+
+    return candidate;
+}
 int main()
 {
     vector<int> nums = {7, 7, 1, 7, 1, 7, 7, 8, 8, 2, 7, 4, 7};
